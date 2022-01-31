bc7c912bba1eef32e0510e07b4c0b170a1fd112b
Camila Lobos <64922748+CamilaLobos@users.noreply.github.com>
2022-01-31 01:40:12 -0300

    Agregué estilos al menú

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
-const divStyle = {
-    backgroundColor: 'cornflowerblue',
-    whidth : '100%',
-    heigth : 'auto',
-
-}
-const links = {
-    backgroundColor : 'cornflowerblue',
-    width : '100%',
-    heigth : 'auto',
-    float : 'inline-end',
-}
-
-const lista = {
-    display: 'flex',
-    justifyContent: 'space-around',
-    float: 'inline-end',
-    width: '40%',
-
-}
-
 export default function NavBar(){
     return (
-        <div style={divStyle}>
+        <div>
             <nav className='barraNavegacion' >
-                <div className='contenedorLogo' style={links}>
-                <h2>LUMINA</h2>
-                    <ul style={lista}>
+                <div className='contenedorLogo'>
+                    <h2>LUMINA</h2>
+                </div>
+                <div className='contenedorLista'>
+                    <ul className='lista'>
                         <li> <a href="#">Inicio</a> </li>
                         <li> <a href="#">Servicios</a> </li>
                         <li> <a href="#">Contacto</a> </li>
